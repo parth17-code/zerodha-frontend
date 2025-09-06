63691f03d2dd68c48e1a903de9643fe57b809103
parth17-code <2024.parth.mohite@ves.ac.in>
2025-09-06 12:57:40 +0530

    quotes from sigmupComponent removed

--- src/landingPage/Signup/SingupComponent.js
+++ src/landingPage/Signup/SingupComponent.js
@@ -30,8 +30,8 @@ function SignupComponent({onSwicth}) {
       if (res.data.success) {
         console.log("Signup successful!");
         setTimeout(() => {
-          window.location.href = "DASHBOARD_URL";
-        }, 2000)
+          window.location.href = DASHBOARD_URL;
+        }, 1000)
       } else {
         console.log(res.data.message);
       }
